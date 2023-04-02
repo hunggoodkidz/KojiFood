958752e0c5efb6e998b954fb386678a2db6a2d1e
hunggoodkidz <ninbook0708@gmail.com>
2023-04-02 15:11:48 +0700

    setup momo payment integration api

--- checkout.php
+++ checkout.php
@@ -3,6 +3,10 @@
 <?php
 include("connection/connect.php");
 include_once 'product-action.php';
+
+
+
+
 error_reporting(0);
 session_start();
 if(empty($_SESSION["user_id"]))
@@ -10,51 +14,115 @@ if(empty($_SESSION["user_id"]))
 	header('location:login.php');
 }
 else{
-
-										  
-												foreach ($_SESSION["cart_item"] as $item)
-												{
+	  
+		foreach ($_SESSION["cart_item"] as $item)
+		{
 											
-												$item_total += ($item["price"]*$item["quantity"]);
-												
-													if($_POST['submit'])
-													{
-						
-													$SQL="insert into users_orders(u_id,title,quantity,price) values('".$_SESSION["user_id"]."','".$item["title"]."','".$item["quantity"]."','".$item["price"]."')";
-						
-														mysqli_query($db,$SQL);
-														
-														$success = "Thankyou! Your Order Placed successfully!";
+            $item_total += ($item["price"]*$item["quantity"]);
 
-														
-														
-													}
-												}
+            if($_POST['submit']) {
+                if($_POST['mod'] == 'COD') {
+                    $SQL="insert into users_orders(u_id,title,quantity,price) values('".$_SESSION["user_id"]."','".$item["title"]."','".$item["quantity"]."','".$item["price"]."')";
+                            
+                    mysqli_query($db,$SQL);
+                                                                    
+                    $success = "Thankyou! Your Order Placed successfully!";
+                } else if($_POST['mod'] == 'momo') {
+                    header('Content-type: text/html; charset=utf-8');
+                  // code to process momo payment
+                  function execPostRequest($url, $data)
+                    {
+                        $ch = curl_init($url);
+                        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+                        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                                'Content-Type: application/json',
+                                'Content-Length: ' . strlen($data))
+                        );
+                        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+                        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+                        //execute post
+                        $result = curl_exec($ch);
+                        //close connection
+                        curl_close($ch);
+                        return $result;
+                    }
+
+                  $endpoint = "https://test-payment.momo.vn/v2/gateway/api/create";
+
+                  $partnerCode = 'MOMOBKUN20180529';
+                  $accessKey = 'klm05TvNBzhg7h7j';
+                  $secretKey = 'at67qH6mk8w5Y1nAyMoYKMWACiEi2bsa';
+                  $orderInfo = "Thanh toán qua MoMo";
+                  $amount = "10000";
+                  $orderId = rand(00,9999);
+                  $redirectUrl = "http://localhost/KojiFood/thankyou.php";
+                  $ipnUrl = "http://localhost/KojiFood/thankyou.php";
+                  $extraData = "";
+                  
+                  $partnerCode = $partnerCode;
+                  $accessKey = $accessKey;
+                  $serectkey = $secretKey;
+                  $orderId = $orderId; // Mã đơn hàng
+                  $orderInfo = $orderInfo;
+                  $amount = $amount;
+                  $ipnUrl = $ipnUrl;
+                  $redirectUrl = $redirectUrl;
+                  $extraData = $extraData;
+
+                      $requestId = time() . "";
+                      $requestType = "payWithATM";
+                      //$extraData = ($_POST["extraData"] ? $_POST["extraData"] : "");
+                      //before sign HMAC SHA256 signature
+                      $rawHash = "accessKey=" . $accessKey . "&amount=" . $amount . "&extraData=" . $extraData . "&ipnUrl=" . $ipnUrl . "&orderId=" . $orderId . "&orderInfo=" . $orderInfo . "&partnerCode=" . $partnerCode . "&redirectUrl=" . $redirectUrl . "&requestId=" . $requestId . "&requestType=" . $requestType;
+                      $signature = hash_hmac("sha256", $rawHash, $serectkey);
+                      $data = array('partnerCode' => $partnerCode,
+                          'partnerName' => "Test",
+                          "storeId" => "MomoTestStore",
+                          'requestId' => $requestId,
+                          'amount' => $amount,
+                          'orderId' => $orderId,
+                          'orderInfo' => $orderInfo,
+                          'redirectUrl' => $redirectUrl,
+                          'ipnUrl' => $ipnUrl,
+                          'lang' => 'vi',
+                          'extraData' => $extraData,
+                          'requestType' => $requestType,
+                          'signature' => $signature);
+                      $result = execPostRequest($endpoint, json_encode($data));
+                      $jsonResult = json_decode($result, true);  // decode json
+
+                    // Insert data into the database after MoMo payment
+                    //   if ($jsonResult['errorCode'] == 0 && $jsonResult['localMessage'] == 'Success') {
+                    //         $SQL="insert into users_orders(u_id,title,quantity,price) values('".$_SESSION["user_id"]."','".$item["title"]."','".$item["quantity"]."','".$item["price"]."')";
+                    //         mysqli_query($db,$SQL);
+                    //     }
+
+                        
+                      header('Location: ' . $jsonResult['payUrl']);
+                  
+                }
+              }                      
+		}
 ?>
-
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="#">
     <title>Starter Template for Bootstrap</title>
-    <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/font-awesome.min.css" rel="stylesheet">
     <link href="css/animsition.min.css" rel="stylesheet">
     <link href="css/animate.css" rel="stylesheet">
-    <!-- Custom styles for this template -->
     <link href="css/style.css" rel="stylesheet"> </head>
 <body>
     
     <div class="site-wrapper">
-        <!--header starts-->
         <header id="header" class="header-scroll top-header headrom">
-            <!-- .navbar -->
             <nav class="navbar navbar-dark">
             <div class="container">
                 <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
@@ -63,18 +131,15 @@ else{
                     <ul class="nav navbar-nav">
                         <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
                         <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li>
-                        
-                    
+
                         <?php
-                    if(empty($_SESSION["user_id"])) // if user is not login
+                    if(empty($_SESSION["user_id"])) 
                         {
                             echo '<li class="nav-item"><a href="login.php" class="nav-link active">Sign In</a> </li>
                         <li class="nav-item"><a href="registration.php" class="nav-link active">Register</a> </li>';
                         }
                     else
                         {
-                                //if user is login
-                                
                                 echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">Orders</a> </li>';
                                 echo '<li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle active" href="#" id="userDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user"></i> '.$_SESSION["username"].'</a>
@@ -96,7 +161,6 @@ else{
                 </div>
             </div>
         </nav>
-            <!-- /.navbar -->
         </header>
         <div class="page-wrapper">
             <div class="top-links">
@@ -114,21 +178,14 @@ else{
                  
 					   <span style="color:green;">
 								<?php echo $success; ?>
-										</span>
-					
+										</span>			
                 </div>
-            
-			
-			
-				  
             <div class="container m-t-30">
 			<form action="" method="post">
                 <div class="widget clearfix">
-                    
                     <div class="widget-body">
                         <form method="post" action="#">
                             <div class="row">
-                                
                                 <div class="col-sm-12">
                                     <div class="cart-totals margin-b-20">
                                         <div class="cart-totals-title">
@@ -137,9 +194,6 @@ else{
 										
                                             <table class="table">
 											<tbody>
-                                          
-												 
-											   
                                                     <tr>
                                                         <td>Cart Subtotal</td>
                                                         <td> <?php echo "$".$item_total; ?></td>
@@ -153,14 +207,9 @@ else{
                                                         <td class="text-color"><strong> <?php echo "$".$item_total; ?></strong></td>
                                                     </tr>
                                                 </tbody>
-												
-												
-												
-												
                                             </table>
                                         </div>
                                     </div>
-                                    <!--cart summary-->
                                     <div class="payment-option">
                                         <ul class=" list-unstyled">
                                             <li>
@@ -170,18 +219,19 @@ else{
                                             </li>
                                             <li>
                                                 <label class="custom-control custom-radio  m-b-10">
-                                                    <input name="mod"  type="radio" value="paypal" disabled class="custom-control-input"> <span class="custom-control-indicator"></span> <span class="custom-control-description">Paypal <img src="images/paypal.jpg" alt="" width="90"></span> </label>
+                                                    <input name="mod"  type="radio" value="momo"  class="custom-control-input"> <span class="custom-control-indicator"></span> <span class="custom-control-description">Thanh Toán Momo <img src="images/momo.jpg" alt="" width="18"></span> </label>
                                             </li>
+
                                         </ul>
                                         <p class="text-xs-center"> <input type="submit" onclick="return confirm('Are you sure?');" name="submit"  class="btn btn-outline-success btn-block" value="Order now"> </p>
                                     </div>
-									</form>
+						</form>
                                 </div>
                             </div>
                        
                     </div>
                 </div>
-				 </form>
+			</form>
             </div>
             <section class="app-section">
                 <div class="app-wrap">
@@ -210,10 +260,8 @@ else{
                     </div>
                 </div>
             </section>
-            <!-- start: FOOTER -->
             <footer class="footer">
                 <div class="container">
-                    <!-- top footer statrs -->
                     <div class="row top-footer">
                         <div class="col-xs-12 col-sm-3 footer-logo-block color-gray">
                             <a href="#"> <img src="images/koji.png" alt="Footer logo"> </a> <span>Order Delivery &amp; Take-Out </span> </div>
@@ -262,8 +310,6 @@ else{
                             </ul>
                         </div>
                     </div>
-                    <!-- top footer ends -->
-                    <!-- bottom footer statrs -->
                     <div class="row bottom-footer">
                         <div class="container">
                             <div class="row">
@@ -298,16 +344,14 @@ else{
                             </div>
                         </div>
                     </div>
-                    <!-- bottom footer ends -->
+
                 </div>
             </footer>
-            <!-- end:Footer -->
+
         </div>
-        <!-- end:page wrapper -->
+
          </div>
 
-     <!-- Bootstrap core JavaScript
-    ================================================== -->
     <script src="js/jquery.min.js"></script>
     <script src="js/tether.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
---
+++ thankyou.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Thank you for your purchase!</title>
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+	<style>
+		body {
+			font-family: Arial, sans-serif;
+			background-color: #f8f9fa;
+		}
+
+		.container {
+			margin: 50px auto;
+			text-align: center;
+		}
+
+		h1 {
+			font-size: 3rem;
+			margin-bottom: 20px;
+		}
+
+		i {
+			font-size: 5rem;
+			color: green;
+			margin-bottom: 20px;
+		}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<i class="fas fa-check-circle"></i>
+		<h1>Thank you for your purchase!</h1>
+        <p>You can view your order details in the <a href="your_orders.php">Order History</a> section.</p>
+        <p>You can also continue shopping from our <a href="index.php">Home Page</a>.</p>
+        <p>If you have any questions, please contact our customer support.</p>
+	</div>
+</body>
+</html>
