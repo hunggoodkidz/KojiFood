4b5c885d9f9d360621e48c1e6373b1c9868720dc
hunggoodkidz <ninbook0708@gmail.com>
2023-04-02 16:03:57 +0700

    add momo payment with QR Code

--- checkout.php
+++ checkout.php
@@ -55,7 +55,7 @@ else{
                   $accessKey = 'klm05TvNBzhg7h7j';
                   $secretKey = 'at67qH6mk8w5Y1nAyMoYKMWACiEi2bsa';
                   $orderInfo = "Thanh toán qua MoMo";
-                  $amount = "10000";
+                  $amount = $item_total;
                   $orderId = rand(00,9999);
                   $redirectUrl = "http://localhost/KojiFood/thankyou.php";
                   $ipnUrl = "http://localhost/KojiFood/thankyou.php";
@@ -102,9 +102,81 @@ else{
                         
                       header('Location: ' . $jsonResult['payUrl']);
                   
-                }
+                }else if($_POST['mod'] == 'momoqrcode'){
+
+
+                    function execPostRequest2($url, $data)
+                    {
+                        $ch = curl_init($url);
+                        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+                        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                                'Content-Type: application/json',
+                                'Content-Length: ' . strlen($data))
+                        );
+                        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+                        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+                        //execute post
+                        $result = curl_exec($ch);
+                        //close connection
+                        curl_close($ch);
+                        return $result;
+                    }
+
+
+                        $endpoint = "https://test-payment.momo.vn/v2/gateway/api/create";
+
+
+                        $partnerCode = 'MOMOBKUN20180529';
+                        $accessKey = 'klm05TvNBzhg7h7j';
+                        $secretKey = 'at67qH6mk8w5Y1nAyMoYKMWACiEi2bsa';
+                        $orderInfo = "Thanh toán qua MoMo";
+                        $amount = $item_total;
+                        $orderId = rand(00,9999);
+                        $redirectUrl = "http://localhost/KojiFood/thankyou.php";
+                        $ipnUrl = "http://localhost/KojiFood/thankyou.php";
+                        $extraData = "";
+
+
+                        $partnerCode = $partnerCode;
+                        $accessKey = $accessKey;
+                        $serectkey = $secretKey;
+                        $orderId = $orderId; // Mã đơn hàng
+                        $orderInfo = $orderInfo;
+                        $amount = $amount;
+                        $ipnUrl = $ipnUrl;
+                        $redirectUrl = $redirectUrl;
+                        $extraData = $extraData;
+                        $requestId = time() . "";
+                        $requestType = "captureWallet";
+                        //$extraData = ($_POST["extraData"] ? $_POST["extraData"] : "");
+                        //before sign HMAC SHA256 signature
+                        $rawHash = "accessKey=" . $accessKey . "&amount=" . $amount . "&extraData=" . $extraData . "&ipnUrl=" . $ipnUrl . "&orderId=" . $orderId . "&orderInfo=" . $orderInfo . "&partnerCode=" . $partnerCode . "&redirectUrl=" . $redirectUrl . "&requestId=" . $requestId . "&requestType=" . $requestType;
+                        $signature = hash_hmac("sha256", $rawHash, $serectkey);
+                        $data = array('partnerCode' => $partnerCode,
+                            'partnerName' => "Test",
+                            "storeId" => "MomoTestStore",
+                            'requestId' => $requestId,
+                            'amount' => $amount,
+                            'orderId' => $orderId,
+                            'orderInfo' => $orderInfo,
+                            'redirectUrl' => $redirectUrl,
+                            'ipnUrl' => $ipnUrl,
+                            'lang' => 'vi',
+                            'extraData' => $extraData,
+                            'requestType' => $requestType,
+                            'signature' => $signature);
+                        $result = execPostRequest2($endpoint, json_encode($data));
+                        $jsonResult = json_decode($result, true);  // decode json
+
+                        //Just a example, please check more in there
+
+                        header('Location: ' . $jsonResult['payUrl']);
+                
               }                      
 		}
+    }
 ?>
 <head>
     <meta charset="utf-8">
@@ -204,7 +276,7 @@ else{
                                                     </tr>
                                                     <tr>
                                                         <td class="text-color"><strong>Total</strong></td>
-                                                        <td class="text-color"><strong> <?php echo "$".$item_total; ?></strong></td>
+                                                        <td class="text-color"><strong> <?php echo $item_total."đ"; ?></strong></td>
                                                     </tr>
                                                 </tbody>
                                             </table>
@@ -220,6 +292,10 @@ else{
                                             <li>
                                                 <label class="custom-control custom-radio  m-b-10">
                                                     <input name="mod"  type="radio" value="momo"  class="custom-control-input"> <span class="custom-control-indicator"></span> <span class="custom-control-description">Thanh Toán Momo <img src="images/momo.jpg" alt="" width="18"></span> </label>
+                                            </li>
+                                            <li>
+                                                <label class="custom-control custom-radio  m-b-10">
+                                                    <input name="mod"  type="radio" value="momoqrcode"  class="custom-control-input"> <span class="custom-control-indicator"></span> <span class="custom-control-description">Thanh Toán Momo QR CODE <img src="images/momo.jpg" alt="" width="18"></span> </label>
                                             </li>
 
                                         </ul>
