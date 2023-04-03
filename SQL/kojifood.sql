cee06fe972e25543cc626fd32167e944af55cfbb
hunggoodkidz <ninbook0708@gmail.com>
2023-04-03 11:14:01 +0700

    update sql file

--- SQL/kojifood.sql
+++ SQL/kojifood.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 6.0.0-dev+20230304.e7bc504387
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 08, 2023 at 05:19 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.4
+-- Generation Time: Apr 03, 2023 at 06:13 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `online_rest`
+-- Database: `kojifood`
 --
 
 -- --------------------------------------------------------
@@ -34,14 +34,15 @@ CREATE TABLE `admin` (
   `email` varchar(222) NOT NULL,
   `code` varchar(222) NOT NULL,
   `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `admin`
 --
 
 INSERT INTO `admin` (`adm_id`, `username`, `password`, `email`, `code`, `date`) VALUES
-(6, 'admin', '81dc9bdb52d04dc20036dbd8313ed055', 'admin@gmail.com', '', '2023-03-07 07:36:18');
+(6, 'admin', '81dc9bdb52d04dc20036dbd8313ed055', 'admin@gmail.com', '', '2023-03-07 07:36:18'),
+(9, 'ninzy', 'e10adc3949ba59abbe56e057f20f883e', 'ninbook0708@gmail.com', 'QFE6ZM', '2023-04-03 02:35:09');
 
 -- --------------------------------------------------------
 
@@ -52,7 +53,7 @@ INSERT INTO `admin` (`adm_id`, `username`, `password`, `email`, `code`, `date`) 
 CREATE TABLE `admin_codes` (
   `id` int(222) NOT NULL,
   `codes` varchar(6) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `admin_codes`
@@ -75,24 +76,28 @@ INSERT INTO `admin_codes` (`id`, `codes`) VALUES
 CREATE TABLE `dishes` (
   `d_id` int(222) NOT NULL,
   `rs_id` int(222) NOT NULL,
-  `title` varchar(222) CHARACTER SET utf8mb4 NOT NULL,
-  `slogan` varchar(222) CHARACTER SET utf8mb4 NOT NULL,
+  `title` varchar(222) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `slogan` varchar(222) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `price` decimal(10,2) NOT NULL,
   `img` varchar(222) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `dishes`
 --
 
 INSERT INTO `dishes` (`d_id`, `rs_id`, `title`, `slogan`, `price`, `img`) VALUES
-(11, 48, 'Bonefish', 'Ba lạng cá rô phi tươi tẩm gia vị nhẹ\n', 55.77, '5ad7582e2ec9c.jpg'),
-(12, 48, 'Hard Rock Cafe', 'Hỗn hợp xà lách cắt nhỏ, phô mai vụn, gà viên', 22.12, '5ad7590d9702b.jpg'),
-(13, 49, 'Uno Pizzeria & Grill', 'Trẻ em có thể chọn hình dạng mì ống, loại nước sốt, loại rau yêu thích', 12.35, '5ad7597aa0479.jpg'),
-(14, 50, 'Red Robins Chick on a Stick', 'Ức gà nướng truyền thống\n', 34.99, '5ad759e1546fc.jpg'),
-(15, 51, 'Lyfe Kitchens Tofu Taco', 'Vegetarian and vegan choices', 11.99, '5ad75a1869e93.jpg'),
-(16, 52, 'Houlihans Mini Cheeseburger', 'Thịt bò hảo hạng', 22.55, '5ad75a5dbb329.jpg'),
-(17, 53, 'Noudle Soup', 'Great taste', 17.99, '5ad79fcf59e66.jpg');
+(11, 48, 'Bonefish', 'Ba lạng cá rô phi tươi tẩm gia vị nhẹ\n', '30000.00', '5ad7582e2ec9c.jpg'),
+(12, 48, 'Hard Rock Cafe', 'Hỗn hợp xà lách cắt nhỏ, phô mai vụn, gà viên', '20000.00', '5ad7590d9702b.jpg'),
+(13, 49, 'Uno Pizzeria & Grill', 'Trẻ em có thể chọn hình dạng mì ống, loại nước sốt, loại rau yêu thích', '30000.00', '5ad7597aa0479.jpg'),
+(14, 50, 'Red Robins Chick on a Stick', 'Ức gà nướng truyền thống\n', '30000.00', '5ad759e1546fc.jpg'),
+(15, 51, 'Lyfe Kitchens Tofu Taco', 'Vegetarian and vegan choices', '20000.00', '5ad75a1869e93.jpg'),
+(16, 52, 'Houlihans Mini Cheeseburger', 'Thịt bò hảo hạng', '30000.00', '5ad75a5dbb329.jpg'),
+(17, 53, 'Noudle Soup', 'Great taste', '20000.00', '5ad79fcf59e66.jpg'),
+(18, 54, 'Cá Chiên', 'cá chiên giòn thơm ngon', '25000.00', '642a4ebbc9c36.jpg'),
+(19, 54, 'Cá Thu', 'Cá thu chiên thơm chua ngon ngọt', '25000.00', '642a4efdbaa4e.jpg'),
+(20, 54, 'Cá nướng ', 'Nướng trộn với chanh muối', '25000.00', '642a4f847f96d.jpg'),
+(21, 54, 'Cá Hồi Nướng', 'Cá hồi bổ sung nhiều protein và chất đạm', '30000.00', '642a5036c300d.jpg');
 
 -- --------------------------------------------------------
 
@@ -103,10 +108,10 @@ INSERT INTO `dishes` (`d_id`, `rs_id`, `title`, `slogan`, `price`, `img`) VALUES
 CREATE TABLE `remark` (
   `id` int(11) NOT NULL,
   `frm_id` int(11) NOT NULL,
-  `status` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
-  `remark` mediumtext CHARACTER SET utf8mb4 NOT NULL,
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `remark` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `remarkDate` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `remark`
@@ -117,7 +122,10 @@ INSERT INTO `remark` (`id`, `frm_id`, `status`, `remark`, `remarkDate`) VALUES
 (63, 32, 'closed', 'cc', '2023-03-03 17:36:46'),
 (64, 32, 'in process', 'fff', '2023-03-03 18:01:37'),
 (65, 32, 'closed', 'hi', '2023-03-03 18:08:55'),
-(66, 34, 'in process', 'on a way', '2023-03-03 18:56:32');
+(66, 34, 'in process', 'on a way', '2023-03-03 18:56:32'),
+(71, 39, 'closed', 'Giao rồi nha cậu', '2023-04-03 03:30:12'),
+(72, 39, 'rejected', 'Ui zoi oi', '2023-04-03 03:35:33'),
+(73, 39, 'closed', 'Oke ', '2023-04-03 03:36:44');
 
 -- --------------------------------------------------------
 
@@ -128,17 +136,17 @@ INSERT INTO `remark` (`id`, `frm_id`, `status`, `remark`, `remarkDate`) VALUES
 CREATE TABLE `restaurant` (
   `rs_id` int(222) NOT NULL,
   `c_id` int(222) NOT NULL,
-  `title` varchar(222) CHARACTER SET utf8mb4 NOT NULL,
-  `email` varchar(222) CHARACTER SET utf8mb4 NOT NULL,
-  `phone` varchar(222) CHARACTER SET utf8mb4 NOT NULL,
-  `url` varchar(222) CHARACTER SET utf8mb4 NOT NULL,
-  `o_hr` varchar(222) CHARACTER SET utf8mb4 NOT NULL,
-  `c_hr` varchar(222) CHARACTER SET utf8mb4 NOT NULL,
-  `o_days` varchar(222) CHARACTER SET utf8mb4 NOT NULL,
-  `address` text CHARACTER SET utf8mb4 NOT NULL,
-  `image` text CHARACTER SET utf8mb4 NOT NULL,
+  `title` varchar(222) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(222) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `phone` varchar(222) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `url` varchar(222) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `o_hr` varchar(222) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `c_hr` varchar(222) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `o_days` varchar(222) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `address` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `image` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `restaurant`
@@ -147,10 +155,11 @@ CREATE TABLE `restaurant` (
 INSERT INTO `restaurant` (`rs_id`, `c_id`, `title`, `email`, `phone`, `url`, `o_hr`, `c_hr`, `o_days`, `address`, `image`, `date`) VALUES
 (48, 5, 'Hari Burger', 'HariBurger@gmail.com', ' 090412 64676', 'HariBurger.com', '7am', '4pm', 'mon-tue', '200 Cô Giang,P.Cô Giang, Q.1', '5ad74ce37c383.jpg', '2023-03-07 18:53:16'),
 (49, 5, 'The Great Kabab Factory', 'kwbab@gmail.com', '011 2677 9070', 'kwbab.com', '6am', '5pm', 'mon-fri', '100 Trần Phú, P.5, Q.10', '5ad74de005016.jpg', '2023-03-07 18:51:19'),
-(50, 6, 'India Taste', 'Indiantaste@gmail.com', '090410 35147', 'Indiantaste.com', '6am', '6pm', 'mon-sat', '200 Bùi Viện, P.Phạm ngũ lão, Q.1', '5ad74e5310ae4.jpg', '2023-03-03 13:55:31'),
+(50, 6, 'Cari Ấn Độ', 'Indiantaste@gmail.com', '090410 35147', 'Indiantaste.com', '6am', '6pm', 'mon-sat', '200 Bùi Viện, P.Phạm ngũ lão, Q.1', '5ad74e5310ae4.jpg', '2023-04-02 15:36:25'),
 (51, 7, 'Martini Food', 'martin@gmail.com', '3454345654', 'martin.com', '8am', '4pm', 'mon-thu', '219 Tôn Thất Thuyết,P.15,Q.4', '5ad74ebf1d103.jpg', '2023-03-01 13:57:19'),
-(52, 8, 'ThanhChicken', 'thanh@gmail.com', '2345434567', 'thanhchicken.com', '6am', '7pm', 'mon-fri', '400 Nguyễn Đính Chiễu,Quận 3', '5ad756f1429e3.jpg', '2023-03-08 14:32:17'),
-(53, 9, 'Kari Store', 'kari@gmail.com', '4512545784', 'kari.com', '7am', '7pm', 'mon-sat', '20 Hồ Xuân Hương, Quận 3', '5ad79e7d01c5a.jpg', '2023-03-04 19:37:33');
+(52, 8, 'ThanhCorn\'s Chicken', 'thanh@gmail.com', '2345434567', 'thanhchicken.com', '6am', '7pm', 'mon-fri', '400 Nguyễn Đính Chiễu,Quận 3', '5ad756f1429e3.jpg', '2023-04-02 15:36:03'),
+(53, 9, 'Mami Store', 'kari@gmail.com', '4512545784', 'kari.com', '7am', '7pm', 'mon-sat', '20 Hồ Xuân Hương, Quận 3', '5ad79e7d01c5a.jpg', '2023-04-02 15:36:44'),
+(54, 9, 'Cat Fish Restaurant', 'catfish@gmail.com', '23141324564', 'catfish.com', '6am', '7pm', 'Mon-Sat', 'Ho Chi Minh City', '642a4e52e9f84.jpg', '2023-04-03 03:56:02');
 
 -- --------------------------------------------------------
 
@@ -162,7 +171,7 @@ CREATE TABLE `res_category` (
   `c_id` int(222) NOT NULL,
   `c_name` varchar(222) NOT NULL,
   `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `res_category`
@@ -192,14 +201,15 @@ CREATE TABLE `users` (
   `address` text NOT NULL,
   `status` int(222) NOT NULL DEFAULT 1,
   `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`u_id`, `username`, `f_name`, `l_name`, `email`, `phone`, `password`, `address`, `status`, `date`) VALUES
-(33, 'thanh', 'thanh', 'ngo', 'user2@gmail.com', '0903836827', 'e10adc3949ba59abbe56e057f20f883e', '210/ Cô Giang', 1, '2023-03-08 15:53:57');
+(33, 'thanh', 'thanh', 'ngo', 'user2@gmail.com', '0903836827', 'e10adc3949ba59abbe56e057f20f883e', '210/ Cô Giang', 1, '2023-03-08 15:53:57'),
+(34, 'ninzy', 'hung', 'tran', 'ninbook0708@gmail.com', '0979810953', 'c56d0e9a7ccec67b4ea131655038d604', 'Ho Chi Minh City', 1, '2023-04-03 03:50:03');
 
 -- --------------------------------------------------------
 
@@ -215,7 +225,15 @@ CREATE TABLE `users_orders` (
   `price` decimal(10,2) NOT NULL,
   `status` varchar(222) DEFAULT NULL,
   `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `users_orders`
+--
+
+INSERT INTO `users_orders` (`o_id`, `u_id`, `title`, `quantity`, `price`, `status`, `date`) VALUES
+(39, 34, 'Hard Rock Cafe', 1, '20000.00', 'closed', '2023-04-03 03:36:44'),
+(40, 34, 'Hard Rock Cafe', 1, '20000.00', NULL, '2023-04-03 03:43:10');
 
 --
 -- Indexes for dumped tables
@@ -277,7 +295,7 @@ ALTER TABLE `users_orders`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `adm_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `adm_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `admin_codes`
@@ -289,19 +307,19 @@ ALTER TABLE `admin_codes`
 -- AUTO_INCREMENT for table `dishes`
 --
 ALTER TABLE `dishes`
-  MODIFY `d_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `d_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `remark`
 --
 ALTER TABLE `remark`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
 
 --
 -- AUTO_INCREMENT for table `restaurant`
 --
 ALTER TABLE `restaurant`
-  MODIFY `rs_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+  MODIFY `rs_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- AUTO_INCREMENT for table `res_category`
@@ -313,13 +331,13 @@ ALTER TABLE `res_category`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `u_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `u_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `users_orders`
 --
 ALTER TABLE `users_orders`
-  MODIFY `o_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `o_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
